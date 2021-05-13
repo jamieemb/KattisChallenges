5eebbb8d68bc80a79827cb02b8563e4697d6d72d
jettsec <jettsec@protonmail.ch>
2021-05-13 14:05:44 +0100

    210513

--- Templates/src/timeloop.java
+++ Templates/src/timeloop.java
@@ -1,2 +1,14 @@
+import java.util.Scanner;
+
 public class timeloop {
+
+    public static void main(String[] args){
+
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+
+        for (int i=1;i<=n;i++){
+            System.out.printf("%s Abracadabra \n", i);
+        }
+    }
 }
